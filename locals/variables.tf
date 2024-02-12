c098a947ee26c025f92ff989ca92ab1116d2aa0c
phlabsdevops <harinathbeeram1999@gmail.com>
2024-02-12 08:56:36 +0530

    phlabsdevops

---
+++ locals/variables.tf
@@ -0,0 +1,16 @@
+variable "isProd" {
+  type = bool
+  default = true
+}
+
+variable "ami_id" {
+  default = "ami-0f3c7d07486cad139"
+}
+
+variable "zone_id" {
+  default = "Z104317737D96UJVA7NEF"
+}
+
+variable "domain_name" {
+  default = "phlablsdevops.online"
+}
